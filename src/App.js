63561a9f43877ecae22216a0e219ec3497193a6b
Ajarnshilin <ajarnshilin@gmail.com>
2021-01-05 12:32:40 +0700

    Edit app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import routes from './configs/routes'
+import { Reset } from 'styled-reset'
+import Modal from 'react-modal'
 
-function App() {
+Modal.setAppElement('#root')
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Reset />
+      <Router>
+        <Suspense fallback='...loading'>
+          <Switch>
+            {/* <Popup /> */}
+            {Object.keys(routes).map(routeKey => (
+              <Route Key={routeKey} {...routes[routeKey]} />
+            ))}
+          </Switch>
+        </Suspense>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
