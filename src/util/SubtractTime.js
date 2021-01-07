4ea324e8f0cbd5395b6d5210e71525da409dc74f
walax31 <walax31009@gmail.com>
2021-01-07 14:50:44 +0700

    EDIT function

--- src/util/SubtractTime.js
+++ src/util/SubtractTime.js
@@ -1,19 +1,12 @@
+import moment from "moment";
+
 export function SubtractTime(currentDuration,RemovedSong){
-    currentDuration = currentDuration.toString()
-    RemovedSong = RemovedSong.toString()
-    let minute = 0;
-    let second = 0;
-    let secondRemainder = 0;
-    currentDuration = currentDuration.split("  min  ");
-    RemovedSong = RemovedSong.split(".");
-    second = parseFloat(currentDuration[1]) - parseFloat(RemovedSong[1]);
-    secondRemainder = second % 60;
-    minute = parseFloat(currentDuration[0]) - parseFloat(RemovedSong[0]);
-    if (second < 0) {
-      minute--;
-      secondRemainder = second + 60;
-    }
-    currentDuration = minute + "  min  " + secondRemainder;
-  
-    return currentDuration;
+  var momentInTime = moment(currentDuration, "m:ss")
+  .subtract(RemovedSong, "seconds")
+  .subtract(RemovedSong, "minutes")
+  .format("m:ss");
+  momentInTime= momentInTime.split(":")
+  momentInTime =momentInTime[0]+ "min" +momentInTime[1]
+
+  return momentInTime;
 }
