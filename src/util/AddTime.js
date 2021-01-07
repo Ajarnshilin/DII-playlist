240f9c59465bc816c099fade96da8c43b765697e
devillkaow <devillkaow@gmail.com>
2021-01-07 17:59:13 +0700

    Edit function

--- src/util/AddTime.js
+++ src/util/AddTime.js
@@ -6,11 +6,14 @@ export function AddTime(currentDuration,Addedsong){
   Addedsong = Addedsong.toString()
   Addedsong = Addedsong.split(".");
     
-  let momentInTime = moment(currentDuration, 'mm.ss')
+  let momentInTime = moment(currentDuration, 'm.s')
   .add(Addedsong[1], 'seconds')
   .add(Addedsong[0], 'minutes')
-  .format('mm:ss');
+  .format('m:s');
   
+  momentInTime=momentInTime.split(":")
+  momentInTime=momentInTime[0] +' min '+momentInTime[1]
+
     return momentInTime;
   
 }
--- src/util/SubtractTime.js
+++ src/util/SubtractTime.js
@@ -5,11 +5,15 @@ export function SubtractTime(currentDuration,RemovedSong){
   currentDuration = currentDuration.toString()
   RemovedSong = RemovedSong.toString()
   RemovedSong = RemovedSong.split(".");
+  
 
-  let momentInTime = moment(currentDuration, 'mm:ss')
-  .subtract(RemovedSong, 'seconds')
-  .subtract(RemovedSong, 'minutes')
-  .format('mm:ss');
+  let momentInTime = moment(currentDuration, 'm:s')
+  .subtract(RemovedSong[1], 'seconds')
+  .subtract(RemovedSong[0], 'minutes')
+  .format('m:s');
+
+  momentInTime=momentInTime.split(":")
+  momentInTime=momentInTime[0] +' min '+momentInTime[1]
   
   return momentInTime;
 }
